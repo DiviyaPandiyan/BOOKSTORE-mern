38a033e7f0ba49ed107dc8e57a1610a49f90dfe9
Diviya Pandiyan <kdmail9@gmail.com.com>
2024-07-20 12:43:22 +0530

    create route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './page/Editpage';
+import ShowBook from './pages/ShowBook';
 
-export default function App() {
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/books/create' element={<CreateBook/>}/>
+      <Route path='/books/details/:id' element={<ShowBook/>}/>
+      <Route path='/books/edit/:id' element={<EditBook/>}/>
+      <Route path='/books/delete/:id' element={<DeleteBook/>}/>
+      
+    </Routes>
+
   )
 }
+
+export default App
